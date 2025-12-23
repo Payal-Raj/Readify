8146fd3d496acf598b5fb25d2b28f20f65784282
Payal-Raj <piyushikumari35@gmail.com>
2025-12-23 10:26:04 +0500

    Designed Student dashboard

--- src/main/java/com/example/readify/StudentDashboardController.java
+++ src/main/java/com/example/readify/StudentDashboardController.java
@@ -1,4 +1,13 @@
 package com.example.readify;
 
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
 public class StudentDashboardController {
+    @FXML
+    private Text StudentName;
+
+    public void setStudentName(String name) {
+        StudentName.setText(name);
+    }
 }
